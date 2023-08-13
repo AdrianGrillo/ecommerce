4f153022c413ac1377a64770f9db29ac0b4f2f0b
Adrian <grillo.adrian13@gmail.com>
2023-08-13 09:46:27 -0400

    now uses sql server

---
+++ ecommerce_web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Passing DbContextOptions to the class ApplicationDbContext that then pass those options to the base class, DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
