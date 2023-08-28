95ed414c0d110aa7a55948f46cb39dbca8da2611
AdrianGrillo <grillo.adrian13@gmail.com>
2023-08-28 16:34:40 -0400

    added server side validation for category controller

--- ecommerce_web/Controllers/CategoryController.cs
+++ ecommerce_web/Controllers/CategoryController.cs
@@ -25,5 +25,20 @@ namespace ecommerce_web.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            // Only save new row if new category is valid according to its model
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                // Redirect to index after adding new row to categories table
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
